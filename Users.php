fff217ee9d089cf51244f3794d2b539816833f70
mozgovoyandrey <mozgovoy.andrey@gmail.com>
2014-11-02 15:23:55 +0400

    new

---
+++ Users.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace mozgovoyandrey\users;
+
+use Yii;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'mozgovoyandrey\users\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
